81d8136f8d5c2422dc247ebddcbff2e4bdc922a1
anishsana <anishsana91@gmail.com>
2016-08-17 14:56:35 -0400

    Modification

--- js/contact_me.js
+++ js/contact_me.js
@@ -15,6 +15,7 @@ $(function() {
             var email = $("input#email").val();
             var phone = $("input#phone").val();
             var message = $("textarea#message").val();
+            var _gotcha = $("input#_gotcha").val();
             var firstName = name; // For Success/Failure Message
             // Check for white space in name for Success/Fail message
             if (firstName.indexOf(' ') >= 0) {
@@ -29,7 +30,8 @@ $(function() {
                     name: name,
                     phone: phone,
                     email: email,
-                    message: message
+                    message: message,
+                    _gotcha: _gotcha
                 },
                 cache: false,
                 success: function() {
